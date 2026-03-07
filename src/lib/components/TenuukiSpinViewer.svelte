247999a349ad9dc02f0f6829bfc5c28b77faac07
Tyler Pham <tyler@disruptiveart.com>
2026-03-07 14:25:55 -0500

    add spin

--- src/lib/components/TenuukiSpinViewer.svelte
+++ src/lib/components/TenuukiSpinViewer.svelte
@@ -6,13 +6,32 @@ import '../styles/spin-viewer.scss'
 import type { TenuukiSpinViewerOptions } from '$lib/types/TenuukiSpinViewerOptions'
 
 export let options: TenuukiSpinViewerOptions
+let currentFrame = 0
+let frameInterval: number = 100
+let imgElement: HTMLImageElement
+
+$: currentSrc = options?.images?.[currentFrame] || ''
+
 onMount(() => {
   console.log('TenuukiSpinViewer mounted with options:', options)
+
+  if (!options || !options.images || options.images.length === 0) {
+    console.warn('TenuukiSpinViewer: No images provided in options')
+    return
+  }
+
+  const interval = setInterval(() => {
+    currentFrame = (currentFrame + 1) % options.images.length
+  }, frameInterval)
+
+  return () => clearInterval(interval)
 })
 </script>
 
 <div class="tenuuki-spin-viewer">
-  {#each options.images as image}
-    <img src={image} alt="Spinner frame" />
-  {/each}
+  {#if currentSrc}
+    <img bind:this={imgElement} src={currentSrc} alt="Spinner frame {currentFrame}" />
+  {:else}
+    <p>Loading...</p>
+  {/if}
 </div>
